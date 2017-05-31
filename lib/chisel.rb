2bb51483de158cbb17c1511a886e23cad531d098
Michael Centrelli <centm860@newschool.edu>
2017-05-31 14:10:13 -0600

    ARGV working

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -1,14 +1,26 @@
 class Chisel
-  attr_reader :string,
-              :text
+  attr_reader :markdown
 
-  def initialize
-    @open_text =  File.open(ARGV[0], "r")
-    @text = @open_text.read.chomp
+  def initialize(markdown)
+    @markdown = markdown
   end
 
+  def convert_to_html
+    @markdown.reverse
+  end
+
+im_running_the_program = ($PROGRAM_NAME == __FILE__)
+
+if im_running_the_program
+  markdown_file = ARGV[0]
+  html_file = ARGV[1]
+
+  markdown = File.read(markdown_file)
+
+  html = Chisel.new(markdown).convert_to_html
+
+  File.write(html_file, html)
 
 
-
-
+end
 end
--- test/chisel_test.rb
+++ test/chisel_test.rb
@@ -12,10 +12,26 @@ class ChiselTest < MiniTest::Test
   end
 
 
-  def test_it_initializes_input_markdown
-    c = Chisel.new
+  def test_it_can_convert_to_html
+    markdown = '# My Life in Desserts
 
-    assert_equal "This is the first line of the paragraph.", c.text
+    ## Chapter 1: The Beginning
+
+    "You just *have* to try the cheesecake," he said. "Ever since it appeared in
+    **Food & Wine** this place has been packed every night."'
+
+    expected_convert = '<h1>My Life in Desserts</h1>
+
+    <h2>Chapter 1: The Beginning</h2>
+
+    <p>
+    "You just <em>have</em> to try the cheesecake," he said. "Ever since it appeared in
+    <strong>Food &amp; Wine</strong> this place has been packed every night."
+    </p>'
+
+    html = Chisel.new(markdown).convert_to_html
+    assert_equal expected_convert, html
+
   end
 
 
