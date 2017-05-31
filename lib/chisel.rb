4fd450420e5d72638faa9feabd2805d4d08cf27c
Michael Centrelli <centm860@newschool.edu>
2017-05-31 13:12:59 -0600

    First test

---
+++ lib/chisel.rb
@@ -0,0 +1,7 @@
+class Chisel
+
+
+
+
+
+end
---
+++ test/chisel_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/chisel'
+require 'pry'
+
+
+class ChiselTest < MiniTest::Test
+
+  def test_it_exists
+
+    assert Chisel, Chisel.new
+  end
+
+
+
+
+
+
+
+end
